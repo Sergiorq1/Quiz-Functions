3d4f1873c2c3cf402bd2701c4cff9a1e9801edd5
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-09-09 19:42:14 -0700

    Add files via upload

---
+++ main copy.py
@@ -0,0 +1,23 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+print(result)
+print(area(10,26))
+print(area(10,10))
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+print(subtract(12,4))
+print(subtract(100,98))
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+print(add(50,20))
+print(add(300,40))
